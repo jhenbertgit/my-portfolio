d8913d73cee152eded18e0eca23aeecedd14d9c8
Jhenbert <hello@jhenbert.com>
2022-11-27 13:40:34 +0800

    modified:   README.md 	modified:   package-lock.json 	modified:   package.json 	modified:   public/index.html 	deleted:    public/logo192.png 	deleted:    public/logo512.png 	deleted:    public/manifest.json 	deleted:    public/robots.txt 	modified:   src/App.css 	modified:   src/App.js 	deleted:    src/App.test.js 	new file:   src/components/About/About.css 	new file:   src/components/About/About.js 	new file:   src/components/Contact/Contact.css 	new file:   src/components/Contact/Contact.js 	new file:   src/components/Footer/Footer.css 	new file:   src/components/Footer/Footer.js 	new file:   src/components/Header/Header.css 	new file:   src/components/Header/Header.js 	new file:   src/components/Navbar/Navbar.css 	new file:   src/components/Navbar/Navbar.js 	new file:   src/components/ProjectContainer/ProjectContainer.css 	new file:   src/components/ProjectContainer/ProjectContainer.js 	new file:   src/components/Projects/Projects.css 	new file:   src/components/Projects/Projects.js 	new file:   src/components/ScrollToTop/ScrollToTop.css 	new file:   src/components/ScrollToTop/ScrollToTop.js 	new file:   src/components/Skills/Skills.css 	new file:   src/components/Skills/Skills.js 	new file:   src/contexts/theme.js 	modified:   src/index.css 	modified:   src/index.js 	deleted:    src/logo.svg 	new file:   src/portfolio.js 	deleted:    src/reportWebVitals.js 	deleted:    src/setupTests.js

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => (
+  <footer className="footer">
+    <a href="http://jhenbert.com" className="link footer__link">
+      Created By Jhenbert Villamucho
+    </a>
+  </footer>
+);
+
+export default Footer;
---
+++ src/portfolio.js
@@ -0,0 +1,65 @@
+const header = {
+  // all the properties are optional - can be left empty or deleted
+  homepage: "#",
+  title: "JS.",
+};
+
+const about = {
+  // all the properties are optional - can be left empty or deleted
+  name: "Jhenbert Villamucho",
+  role: "Full Stack Web Developer",
+  description:
+    "Building stunning webapps that can help your company grow and reach millions of audience.",
+  resume: "http://resume.jhenbert.com/",
+  social: {
+    facebook: "https://facebook.com",
+    github: "https://github.com",
+  },
+};
+
+const projects = [
+  // projects can be added an removed
+  // if there are no projects, Projects section won't show up
+  {
+    name: "Project 1",
+    description: "Sample project",
+    stack: ["React", "Material-UI"],
+    sourceCode: "https://github.com",
+    livePreview: "https://github.com",
+  },
+  {
+    name: "Project 2",
+    description: "Sample project",
+    stack: ["React", "Material-UI"],
+    sourceCode: "https://github.com",
+    livePreview: "https://github.com",
+  },
+  {
+    name: "Project 3",
+    description: "Sample project",
+    stack: ["React", "Material-UI"],
+    sourceCode: "https://github.com",
+    livePreview: "https://github.com",
+  },
+];
+
+const skills = [
+  // skills can be added or removed
+  // if there are no skills, Skills section won't show up
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "React",
+  "Material UI",
+  "Git",
+  "Node JS",
+  "Java",
+  "Python",
+];
+
+const contact = {
+  // email is optional - if left empty Contact section won't show up
+  email: "hello@mail.com",
+};
+
+export { header, about, projects, skills, contact };
