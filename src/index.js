29cad228e0f7f651cb365b6813f3e56814df4c8e
Jhenbert Villamucho <jvv205756@gmail.com>
2023-05-01 01:17:10 +0800

    fix error in render

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { render } from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ThemeProvider } from "./contexts/theme";
 import "./index.css";
 
-render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <ThemeProvider>
     <App />
-  </ThemeProvider>,
-  document.getElementById("root")
+  </ThemeProvider>
 );
